aeff44f5877b2de0cfa40abba656d92aca21b16c
Parker Mauney <parkermauney@gmail.com>
2019-09-08 15:51:45 -0500

    Add fs mock module

---
+++ __mocks__/fs.js
@@ -0,0 +1,42 @@
+// https://jestjs.io/docs/en/manual-mocks#mocking-node-modules
+'use strict';
+
+const path = require('path');
+
+const fs = jest.genMockFromModule('fs');
+
+// This is a custom function that our tests can use during setup to specify
+// what the files on the "mock" filesystem should look like when any of the
+// `fs` APIs are used.
+let mockFiles = Object.create(null);
+function __setMockFiles(newMockFiles) {
+  mockFiles = Object.create(null);
+  for (const file in newMockFiles) {
+    const dir = path.dirname(file);
+
+    if (!mockFiles[dir]) {
+      mockFiles[dir] = [];
+    }
+    mockFiles[dir].push(path.basename(file));
+  }
+}
+
+// A custom version of `readdirSync` that reads from the special mocked out
+// file list set via __setMockFiles
+function readdirSync(directoryPath) {
+  return mockFiles[directoryPath] || [];
+}
+
+function existsSync(directoryPath) {
+  return !!mockFiles[directoryPath];
+}
+
+function readFileSync(filePath, options) {
+  return mockFiles[filePath];
+}
+
+fs.__setMockFiles = __setMockFiles;
+fs.existsSync = existsSync;
+fs.readdirSync = readdirSync;
+fs.readFileSync = readFileSync;
+module.exports = fs;
